c4eba86686a3dd0fbf94c8b25b1e31d4f3ad5911
Lorenzbeto <lorenzo96beto@gmail.com>
2023-05-26 09:28:48 -0600

    Quedo
    Ya se entrego

--- Productos/models.py
+++ Productos/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Producto(models.Model):
     titulo=models.CharField(max_length=50,verbose_name="Titulo")
-    contenido=models.CharField(max_length=50,verbose_name="Contenido")
+    contenido=models.CharField(max_length=500,verbose_name="Contenido")
     imagen=models.ImageField(verbose_name="Imagen",upload_to='productos' )
     created=models.DateTimeField(auto_now_add=True,verbose_name="Contenido")
     updated=models.DateTimeField(auto_now_add=True,verbose_name="Contenido")
